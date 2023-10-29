4a97e3a48b84ef1c99ca7147b525458ed9f46387
DoDangDucCoder <ducheebbaby@gmail.com>
2023-10-29 17:43:22 +0700

    first commit

---
+++ ModemDesign/ModemDesign/MVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ModemDesign.Core;
+
+
+namespace ModemDesign.MVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currenView;
+
+        public object CurrenView 
+        {
+            get { return _currenView; }
+            set 
+            {
+                _currenView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel() 
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrenView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrenView = HomeVM;
+            });
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrenView = DiscoveryVM;
+            });
+        }
+    }
+}
